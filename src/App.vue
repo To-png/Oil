89feac0ac56591d6d74f89d7b4605eb6dd045211
Dmi Try <d.a.lap@mail.ru>
2021-09-22 19:29:36 +0300

    msg

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app">    
+    <a-layout>
+      <a-layout-sider>
+        <div id="nav">
+          <div>
+            <router-link to="/">Линии</router-link>
+          </div>
+          <div>
+            <router-link to="/settings">Настройки</router-link>
+          </div>
+        </div>
+      </a-layout-sider>
+      <a-layout>         
+        <a-layout-content>
+          <router-view/>
+        </a-layout-content>          
+      </a-layout>
+    </a-layout>       
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#app {  
+  text-align: left;   
+}
+
+#app .ant-layout-sider {
+  background: #3ba0e9;
+  color: #fff;
+  line-height: 30px;
+  height: 100vh; 
+}
+
+#app .ant-layout-content {
+  background: rgba(16, 142, 233, 1);
+  color: #fff;
+  padding: 30px;  
+}
+
+#app > .ant-layout {
+  margin-bottom: 48px;
+}
+
+#app > .ant-layout:last-child {
+  margin: 0;
 }
 
 #nav {
@@ -27,6 +56,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #fff;
 }
 </style>
